370e15dd114384db762cc1d930e5954639dd065c
Maitri Sheta <147026351+Maitri-Sheta@users.noreply.github.com>
2025-12-16 02:13:43 +0530

    if-elif-else.py

--- basic/05_if-elif-else.py
+++ basic/05_if-elif-else.py
@@ -10,3 +10,20 @@ elif (timestamp >"12:00:00" and timestamp < "18:00:00"):
     print("Good Afternoon")
 else:
     print("Good Evening")
+
+''' short hand if_else
+syntex
+    true_statment if condition else else_statement
+used when the condition being tested is simple and execution of code is short.
+'''
+#short_hand if_else
+a,b=25,36
+print("A") if a>b else ("=") if a==b else print("B")
+#typically way
+if (a>b):
+    print("A")
+else:
+    if (a==b):
+        print("=")
+    else:
+        print("B")
